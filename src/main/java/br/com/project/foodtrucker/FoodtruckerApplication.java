54144657537ba7329cea21edb0b72d26fb7ace38
Felipe Paschoal <luizfelipepaschoal771478@gmail.com>
2025-05-19 19:22:57 -0300

    feature: setup Java project

---
+++ src/main/java/br/com/project/foodtrucker/FoodtruckerApplication.java
@@ -0,0 +1,13 @@
+package br.com.project.foodtrucker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodtruckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodtruckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/project/foodtrucker/FoodtruckerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.foodtrucker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodtruckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
